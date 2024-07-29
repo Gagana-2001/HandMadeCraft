e831bc115e2229dcb14b5aa45f2345a0fa5ba04c
Gagana-2001 <gaganamahadev2001@gmail.com>
2024-07-29 17:05:35 +0530

    Hand Made craft

--- src/main/java/selenium/com/pageobjectModels/PDPPage.java
+++ src/main/java/selenium/com/pageobjectModels/PDPPage.java
@@ -29,7 +29,7 @@ public class PDPPage extends AbstractComponent {
 	@FindBy(xpath = "//div[@data-cart-add-success='theme-cart-add-success']/div[2]/a[1]")
 	private WebElement viewCartBtn;
 
-	@FindBy(xpath = "//div[@class='theme-product-color-variations']/span[1]/label")
+	@FindBy(xpath = "//div[@class='theme-product-color-variations']/span[2]/label")
 	private WebElement colour;
 
 	@FindBy(xpath = "//div[@class='theme-variant-select-container']/span[1]/label")
